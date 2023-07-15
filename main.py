62ed5c30269e97c3223cd6ea822af201c8657197
KrayziCoder11 <john.dumont11@gmail.com>
2023-07-15 16:43:04 -0400

    updated links

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from playsound import playsound
 import speech_recognition
 import webbrowser
 import json
-import os
-
 
 # Initialize the recognizer
 LISTENER = speech_recognition.Recognizer()
@@ -13,16 +11,14 @@ with open("config.json") as file:
     config = json.load(file)
 
 
-# the urls for the websites
-# HASD TO BE THE FULL URL -- GO TO THE WEBSITE AND COPY IT DIRECTLY
+# the urls for the websites or the absolute path for exe files
+# (HAS TO BE THE FULL URL -- GO TO THE WEBSITE AND COPY IT DIRECTLY)
 SCHOOL_LINKS = config["SCHOOL_LINKS"]
 GAMING_LINKS = config["GAMING_LINKS"]
 
 # the absolute path for the program launchers
 # the path has to be for the program executable -- not shortcut
 # have path use / not \
-SCHOOL_PROGRAMS = config["SCHOOL_PROGRAMS"]
-GAMING_PROGRAMS = config["GAMING_PROGRAMS"]
 
 # wav file paths
 GREETING = config["GREETING"]
@@ -91,10 +87,7 @@ def open_links(phrase : str):
     if "school" in phrase:
         for link in SCHOOL_LINKS: #opens links
             webbrowser.open(link)
-
-        for program in SCHOOL_PROGRAMS: #runs programs
-            os.startfile(program)
-
+            
         return "school"
 
     #if user said "game"
@@ -102,10 +95,6 @@ def open_links(phrase : str):
         for link in GAMING_LINKS: #opens links
             webbrowser.open(link)
 
-        for program in GAMING_PROGRAMS: #runs programs
-            
-            os.startfile(program)
-        
         return "game"
 
     elif "no thanks" in phrase:
@@ -115,14 +104,14 @@ def open_links(phrase : str):
         return "empty"
 
 def main():
-    
+    open_links("game")
     # wav file path of the initial audio msg
-    play_audio(GREETING)
+    # play_audio(GREETING)
 
-    user_response = get_user_input()
+    # user_response = get_user_input()
 
-    ai_response = RESPONSE_DICT[user_response]
-    play_audio(ai_response)
+    # ai_response = RESPONSE_DICT[user_response]
+    # play_audio(ai_response)
 
     """
     to do list:
